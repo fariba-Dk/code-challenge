d081dc8b4542f8e33fb44282604d41e486aabd63
Fariba Dadko <fariba.menchies@gmail.com>
2023-10-05 22:56:08 -0700

    Initial commit

---
+++ challenges/6.js
@@ -0,0 +1,29 @@
+// Given two input strings, s1 and s2, write code to check if s2 is a rotation of s1.
+//
+// Ex
+//
+// ```
+// isRotation("coding", "ingcod") // -> true
+// ```
+//
+// ```
+// isRotation("help", "phel") // -> true
+// ```
+
+// ```
+// isRotation("github", "git")// -> false
+// ```
+//
+// ```
+// isRotation("node", "edon") -> false
+// ```
+// Hint: Note that the order of the characters of string 2 matters and is actaully a shift in the index of the characters from string 1. 
+// The letters in string 2 cannot be in a randomized order and still pass true.
+//
+// Bonus: find a solution that can work even when the two strings have duplicate letters.
+
+function isRotation(s1,s2){
+  return // your code here
+}
+
+module.exports = isRotation;
